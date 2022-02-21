c947eae54a43e5e0586977ef2280d783d318f79e
Ash Weston <ash-w@live.com.au>
2022-02-21 19:12:14 +1000

    create employee.js - first test

---
+++ Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require("../lib/Employee");
+const employee = new Employee("ash", "123", "ash-w@live.com.au");
+
+test("test if we can get the constructor values for the employee object", () => {
+  expect(employee.name).toEqual("ash");
+  expect(employee.id).toEqual("123");
+  expect(employee.email).toEqual("ash-w@live.com.au");
+});
+
+test("test if we can get the name from getName() method", () => {
+  expect(employee.getName()).toEqual("ash");
+});
+
+test("test if we can get the id from getID() method", () => {
+  expect(employee.getId()).toEqual("123");
+});
+
+test("test if we can get the email from getEmail() method", () => {
+  expect(employee.getEmail()).toEqual("ash-w@live.com.au");
+});
+
+test("test if we can get the role from getRole() method", () => {
+  expect(employee.getRole()).toBe("Employee");
+});
